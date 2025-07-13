e1f6f16623fee02c5d92db2f25c0cd11b7f84b8d
Fazal Shaik <fazalshaik609@gmail.com>
2025-07-14 02:24:07 +0530

    NOTICKET : Allowing newlines input string case using gsub

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,7 @@ module StringCalculator
   def self.add(string)
     return 0 if string.empty?
     
-    string.split(',').sum(&:to_i)
+    string.gsub("\n", ',').split(',').sum(&:to_i)
   end
 
 end
